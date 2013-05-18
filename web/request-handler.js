3e826043513c4ad7fda3d61a7c17b0fb0b054d4e
Hack Reactor Students <github@hackreactor.com>
2013-05-18 11:20:07 -0700

    Implement GET request on root
    added request header and end function

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,30 @@
 exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
 
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "text/plain"
+};
+
 exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+  console.log('-----------------------------------------');
+
+  var statusCode = 200;
+  var body;
+
+  if(req.method === "GET") {
+    body = '<html><input></html>';
+
+  }
+
+  if(req.method === "POST") {
+
+  }
+
+  res.writeHead(statusCode, headers);
+  res.end(body);
+
+  console.log('handleRequest complete', exports.datadir);
 };
