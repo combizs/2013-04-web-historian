0ae42e685d672c6df2c18ca5934ed811a3e7cead
Hack Reactor Students <github@hackreactor.com>
2013-05-18 15:06:31 -0700

    return saved html data on valid GET request
    returned a 404 if the file did not exist

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,6 @@
 exports.datadir = __dirname + "/../data/sites.txt"; // tests will need to override this.
-fs = require("fs");
+var datastore = __dirname + "/../data/sites/";
+var fs = require("fs");
 var url = require("url");
 
 var headers = {
@@ -27,6 +28,20 @@ exports.handleRequest = function (req, res) {
 
 
   if(req.method === "GET") {
+
+    // If we are going to the root level just return default body.
+    // else, try to get the saved html from our datastore
+    if(urlString) {
+
+      // Do we have a filename in our datastore that corresponds to the path?
+      var check = datastore + urlString;
+      if (fs.existsSync(check)) {
+        body = fs.readFileSync(check, 'utf8');
+      }
+      else {
+        statusCode = 404;
+      }
+    }
   }
 
   if(req.method === "POST") {
@@ -43,7 +58,7 @@ exports.handleRequest = function (req, res) {
     req.on('end', function() {
       messageData = JSON.parse('{"' + decodeURI(messageData).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
       fs.appendFileSync(exports.datadir, messageData.url + '\n', 'utf8', function (err) {
-        console.log('Failed to write to file:', exports.datadir);
+        console.error('Failed to write to file:', exports.datadir);
         if (err) throw err;
       });
     });
