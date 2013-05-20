87392887956486d06b53d3e9338156bc83b05cf1
Hack Reactor Students <github@hackreactor.com>
2013-05-19 20:46:49 -0700

    Checks URL to see if already exists in file.
    cron job organizes each folder every 30 seconds.
    
    want to clean up code even more.

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,7 @@ exports.datadir = __dirname + "/../data/sites.txt"; // tests will need to overri
 var datastore = __dirname + "/../data/sites/";
 var fs = require("fs");
 var url = require("url");
+var querystring = require("querystring");
 
 var headers = {
   "access-control-allow-origin": "*",
@@ -12,60 +13,112 @@ var headers = {
 headers["Content-Type"] = "text/html";
 
 exports.handleRequest = function (req, res) {
-  console.log('-----------------------------------------');
-
-  // remove leading '/' character from url
   var parsedUrl = url.parse(req.url);
   var urlString = parsedUrl.pathname;
   if(urlString[0] === "/") {
     urlString = urlString.substr(1);
   }
 
-  console.log('urlString', urlString);
+  headers["Content-Type"] = "text/html";
+  getBody = fs.readFileSync(__dirname + "/index.html", 'utf8');
 
   var statusCode = 200;
-  var body = "<html><form method='POST'><input name='url' id='url'><input type='submit'></form></html>";
-
-
   if(req.method === "GET") {
+    var getBody;
+    if (req.url === '/style.css') {
+      headers["Content-Type"] = "text/css";
+      getBody = fs.readFileSync(__dirname + "/style.css", 'utf8');
+    }
+    else {
+      if(urlString) {
+        var pathName = urlString.split("/");
+        var check = datastore + urlString;
+        if(pathName.length  > 1){
+          if (fs.existsSync(datastore + urlString)) {
+            getBody += fs.readFileSync(check, 'utf8');
+          }
+          else {
+            getBody += "Nothing found";
+            statusCode = 404;
+          }
+        }
+        else {
+          var files = fs.readdirSync(datastore);
+          var li = '';
+          for (var i = 0; i < files.length; i++){
+            if(files[i] !== ".DS_Store") {
+              data = {
+                file: files[i],
+                pathname: pathName
+              };
+              if (fs.existsSync(datastore + files[i] + "/" + pathName[pathName.length-1])) {
+                li += "<li><a href='" + files[i] + "/" + pathName[pathName.length-1] +"'>" + files[i] +"</a></li>";
+              }
+              else {
+                li += "<li>Nothing Found</li>";
+              }
+            }
+          }
+          // data {
+          //   file: [
+          //   {
+          //     header: urlString
+          //     ul: li
 
-    // If we are going to the root level just return default body.
-    // else, try to get the saved html from our datastore
-    if(urlString) {
-
-      // Do we have a filename in our datastore that corresponds to the path?
-      var check = datastore + urlString;
-      if (fs.existsSync(check)) {
-        body = fs.readFileSync(check, 'utf8');
-      }
-      else {
-        statusCode = 404;
+          //   }
+          //   ]
+          // }
+          getBody += "<h1>Searched Domain: " + urlString + "</h1>";
+          getBody += "<ul>" + li + "</ul>";
+        }
       }
     }
+    res.writeHead(statusCode, headers);
+    res.end(getBody);
+
   }
 
   if(req.method === "POST") {
-
-    statusCode = 302;
+    var postBody = '';
+    statusCode = 200;
     req.setEncoding('utf8');
+    var results;
 
     var messageData = '';
+    var result;
+    var fileContents = fs.readFileSync(exports.datadir, 'utf8');
+    var siteNames = fileContents.split('\n');
 
-    req.on('data', function(data) {
-      messageData += data;
-    });
-
+    req.on('data', function(data) { messageData += data; });
     req.on('end', function() {
-      messageData = JSON.parse('{"' + decodeURI(messageData).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
-      fs.appendFileSync(exports.datadir, messageData.url + '\n', 'utf8', function (err) {
-        console.error('Failed to write to file:', exports.datadir);
-        if (err) throw err;
-      });
+      messageData = querystring.parse(messageData);
+      for(var sites = 0; sites < siteNames.length; sites++){
+        if(siteNames[sites] === messageData.url) {
+          result = true;
+        }
+      }
+      if(result){
+        var files = fs.readdirSync(datastore);
+        var li = '';
+        for (var i = 0, l = files.length; i < l; i++){
+          if(files[i] !== ".DS_Store") {
+            if (fs.existsSync(datastore + files[i] + "/" + messageData.url)) {
+              li += "<li><a href='" + files[i] + "/" + messageData.url +"'>" + files[i] +"</a></li>";
+            }
+            else {
+              li += "<li>Nothing Cached</li>";
+            }
+          }
+        }
+        postBody += "<h1>Searched Domain: " + urlString + "</h1>";
+        postBody += "<ul>" + li + "</ul>";
+      }
+      else {
+        postBody = "<h1>" + messageData.url + " is being added to the list!</h1>";
+        fs.appendFileSync(exports.datadir, messageData.url + '\n', 'utf8');
+      }
+      res.writeHead(statusCode, headers);
+      res.end(postBody);
     });
   }
-
-  res.writeHead(statusCode, headers);
-  res.end(body);
-
-  console.log('handleRequest complete', exports.datadir);
 };
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -5,13 +5,38 @@ exports.readUrls = function(filePath, cb){
   var fileContents = fs.readFileSync(filePath, 'utf8');
   var siteNames = fileContents.split('\n');
 
+  var createDateString = function() {
+    var date = new Date();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    return [year, month, day, hours, minutes, seconds].join("-");
+  };
+
+  // make a single dateString to be used as a foldername
+  // for all our sites
+  var dateString = createDateString();
+
   for (var i = 0; i < siteNames.length; i++) {
-    cb(siteNames[i]);
+    cb(siteNames[i], dateString);
   }
 };
 
-exports.downloadUrl = function(url){
-  http.get(url, __dirname + '/../../data/sites/' + url, function( error, result) {
+exports.downloadUrl = function(url, date){
+  // construct the file/folder name based on the date and url
+  var targetName = __dirname + '/../../data/sites/' + date + '/';
+
+  // creating the folder ensures that it exists
+  fs.mkdir(targetName);
+
+  // the filename is based on the url
+  targetName += url;
+
+  http.get(url, targetName , function( error, result) {
     if (error) {
      console.error(error);
     } else {
